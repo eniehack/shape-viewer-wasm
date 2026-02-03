846ac569ebb374db75439a0ab08b34425a0154aa
Nakaya <eniehack@outlook.jp>
2026-02-03 16:52:00 +0900

    feat(frontend): parseされたshapeファイルを地図上にplotできるように etc.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import init, { greet, parse_shp } from '$lib/wasm/shp_parser';
+	import { MapLibre, GeoJSONSource, CircleLayer } from 'svelte-maplibre-gl';
+	import { onMount } from 'svelte';
+	import type GeoJSON from '@types/geojson';
+
+	let files = $state<FileList | undefined>();
+	let initiated = $state(false);
+	let geojson = $derived.by<GeoJSON.FeatureCollection | undefined>(async () => {
+		if (!initiated || !files) return undefined;
+		for (const file of files) {
+			const buf = await file.arrayBuffer();
+			const bytes = new Uint8Array(buf);
+			return JSON.parse(JSON.stringify(parse_shp(bytes))) as GeoJSON.FeatureCollection;
+		}
+	});
+
+	onMount(async () => {
+		await init();
+		initiated = true;
+	});
+</script>
+
+<button onclick={() => greet()}>press it</button>
+
+<input bind:files type="file" name="file" accept=".shp" />
+
+<MapLibre class="h-[400px]" style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json">
+	{#if typeof geojson !== 'undefined'}
+		<GeoJSONSource data={geojson}>
+			<CircleLayer
+				paint={{
+					'circle-color': '#00ff00',
+					'circle-radius': 8
+				}}
+			/>
+		</GeoJSONSource>
+	{/if}
+</MapLibre>
