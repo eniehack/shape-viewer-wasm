9b3c6027353282824002928add06079d4ccf50a3
Nakaya <eniehack@outlook.jp>
2026-02-03 16:50:53 +0900

    chore(forntend): viteでwasmを扱えるように

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import wasm from 'vite-plugin-wasm';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({ plugins: [tailwindcss(), sveltekit(), wasm()] });
